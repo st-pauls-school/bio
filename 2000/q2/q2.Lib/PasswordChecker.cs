2f380413aed6db2e8e6272240c54d16abf5a794f
Chris Harrison <cah@stpaulsschool.org.uk>
2019-09-08 22:44:24 +0100

    2000 Q1

---
+++ 2000/q2/q2.Lib/PasswordChecker.cs
@@ -0,0 +1,21 @@
+﻿namespace q2.Lib
+{
+    public class PasswordChecker
+    {
+        public static string Check(string stringtocheck)
+        {
+            for(int len = 1; len <= stringtocheck.Length/2; len++)
+            {
+                for(int pos = 0; pos + (2*len) <= stringtocheck.Length; pos++)
+                {
+                    string a = stringtocheck.Substring(pos, len);
+                    string b = stringtocheck.Substring(pos + len, len);
+                    if (a.Equals(b))
+                        return "Rejected";
+                }
+            }
+
+            return "Accepted";
+        }
+    }
+}
---
+++ 2000/q2/q2.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using q2.Lib;
+
+namespace q2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodAccepted()
+        {
+            Assert.AreEqual("Accepted", PasswordChecker.Check("A"));
+            Assert.AreEqual("Accepted", PasswordChecker.Check("LONDON"));
+            Assert.AreEqual("Accepted", PasswordChecker.Check("BIOGRAPHY"));
+            Assert.AreEqual("Accepted", PasswordChecker.Check("APRICOT"));
+        }
+
+        [TestMethod]
+        public void TestMethodRejected()
+        {
+            Assert.AreEqual("Rejected", PasswordChecker.Check("AA"));
+            Assert.AreEqual("Rejected", PasswordChecker.Check("QUININE"));
+            Assert.AreEqual("Rejected", PasswordChecker.Check("RINGRING"));
+            Assert.AreEqual("Rejected", PasswordChecker.Check("COMMITTEE"));
+        }
+    }
+}

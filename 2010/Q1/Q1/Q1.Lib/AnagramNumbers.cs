c017be20fdcf70dedcfd53d57a1c8c5e8c0ad66e
Chris Harrison <cah@stpaulsschool.org.uk>
2019-10-15 14:20:23 +0100

    2010 Q1 C#

---
+++ 2010/Q1/Q1/Q1.Lib/AnagramNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1.Lib
+{
+    public class AnagramNumbers
+    {
+        public static List<int> Generate(ulong value)
+        {
+            List<int> rv = new List<int>();
+            for (ulong g = 2; g < 10; g++)
+                if (Compare(value, g * value))
+                    rv.Add((int)g);
+            return rv;
+        }
+        public static bool Compare(ulong a, ulong b)
+        {
+            List<int> la = a.ToString().Select(c => (int)c).ToList();
+            List<int> lb = b.ToString().Select(c => (int)c).ToList();
+            la.Sort();
+            lb.Sort();
+            return la.SequenceEqual(lb);
+        }
+    }
+}

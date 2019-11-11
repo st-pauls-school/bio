245ee0605a1b1932717bbcd13d9642b6037fe8ca
Chris Harrison <cah@stpaulsschool.org.uk>
2019-11-11 10:06:40 +0000

    Faster Q3 but some incorrect answers
    Need to add in paths to solutions

--- 2012/q3/python/number-ladder.py
+++ 2012/q3/python/number-ladder.py
@@ -1,3 +1,5 @@
+import time
+
 def convert(i):
 	conversion = ['ZERO', 'ONE', 'TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE']
 	s = ''
@@ -8,13 +10,18 @@ def convert(i):
 		i //= 10
 	return  ''.join(sorted(list(s)))
 
+# generate all the unique letter combinations
 def cache(x):
 	return [convert(i) for i in range(x)]
 
+# in effect the number of changes is the n(x) + n(y) - n(x ^ y) (i.e. n (x union y) - n (x intersect y))
+# iterate over x, removing characters from y, counting the number removed 
+# then return the total in x, the new y and the number that should be removed from x 
 def remaining(x, y, cache):
 	x = cache[x] 
 	y = cache[y]
 	removed_from_x = 0
+	# given the lists are sorted, this could be done more efficiently 
 	for i in range(len(x)):
 		f = y.find(x[i])
 		if f == 0:
@@ -28,51 +35,63 @@ def remaining(x, y, cache):
 			removed_from_x += 1
 	return len(x) - removed_from_x + len(y)
 
-def number_ladder(queue, target, cache, visited=[]):
-	src, level = queue.pop(0)
-	visited.append(src)
-	for i in range(1000):
-		if i != src and i not in visited:
-			if remaining(src, i, cache) <= 5:
-				if i == target:
-					return level+1
-				queue.append((i, level+1))
-	return number_ladder(queue, target, cache, visited)
 
+def number_ladder(src, target, limit=1000):
+	maps = cache(limit) 
+	visited=[None]*limit
+	queue = [(src,0)]
+	while len(queue) > 0: 
+		src, level = queue.pop(0)
+		if visited[src] == None:
+			visited[src] = level
+		for i in range(limit):
+			if visited[i] == None:
+				if i != src:
+					if remaining(src, i, maps) <= 5:
+						if i == target:
+							return level+1
+						visited[i]=level+1 
+						queue.append((i, level+1))
+	return -1 
 
-mapping_cache = cache(1000)
-#print('A', number_ladder([(26,0)], 61, mapping_cache) == 1)
-#print('B', number_ladder([(1,0)], 90, mapping_cache) == 1)
-#print('C', number_ladder([(1,0)], 610, mapping_cache) == 2)
+# summarise the test, with a time estimate for each result 
+def number_ladder_checker(src, target, expected):
+	t0 = time.time()
+	actual = number_ladder(src,target) 
+	t1 = time.time()
+	total = t1-t0
+	return "{0} -> {1} {2} == {3} => {4} [{5:.02f}s]".format(src, target, expected, actual, expected==actual, total)
 
-#print(remaining(1,2,mapping_cache))
+print(number_ladder_checker(26, 61, 1))
+print(number_ladder_checker(1, 90, 1))
+print(number_ladder_checker(1, 610, 2))
 
-#print(number_ladder([(1,0)], 2, mapping_cache) == 1)
-#print(number_ladder([(1,0)], 3, mapping_cache) == 2)
-#print(number_ladder([(1,0)], 4, mapping_cache) == 1)
+print(number_ladder_checker(1, 2, 1))
+print(number_ladder_checker(1, 3, 2))
+print(number_ladder_checker(1, 4, 1))
 
-#print(number_ladder([(14,0)], 543, mapping_cache) == 2)
-#print(number_ladder([(5,0)], 75, mapping_cache) == 1)
-#print(number_ladder([(71,0)], 713, mapping_cache) == 1)
+print(number_ladder_checker(14, 543, 2))
+print(number_ladder_checker(5, 75, 1))
+print(number_ladder_checker(71, 713, 1))
 
-#print(number_ladder([(21,0)], 911, mapping_cache) == 2)
-#print(number_ladder([(329,0)], 927, mapping_cache) == 2)
-#print(number_ladder([(66,0)], 71, mapping_cache) == 3)
+print(number_ladder_checker(21, 911, 2))
+print(number_ladder_checker(329, 927, 2))
+print(number_ladder_checker(66, 71, 3))
 
-print(number_ladder([(250,0)], 361, mapping_cache) == 3)
-print(number_ladder([(34,0)], 756, mapping_cache) == 4)
-print(number_ladder([(18,0)], 735, mapping_cache) == 3)
+print(number_ladder_checker(250, 361, 3))
+print(number_ladder_checker(34, 756, 4))
+print(number_ladder_checker(18, 735, 3))
 
-#print(number_ladder([(77,0)], 383, mapping_cache) == 5)
-#print(number_ladder([(48,0)], 677, mapping_cache) == 4)
-#print(number_ladder([(232,0)], 471, mapping_cache) == 4)
+print(number_ladder_checker(77, 383, 5))
+print(number_ladder_checker(48, 677, 4))
+print(number_ladder_checker(232, 471, 4))
 
-#print(number_ladder([(220,0)], 691, mapping_cache) == 5)
-#print(number_ladder([(198,0)], 222, mapping_cache) == 5)
-#print(number_ladder([(410,0)], 666, mapping_cache) == 6)
+print(number_ladder_checker(220, 691, 5))
+print(number_ladder_checker(198, 222, 5))
+print(number_ladder_checker(410, 666, 6))
 
-#print(number_ladder([(402,0)], 788, mapping_cache) == 6)
-#print(number_ladder([(203,0)], 959, mapping_cache) == 6)
-#print(number_ladder([(404,0)], 777, mapping_cache) == 6)
+print(number_ladder_checker(402, 788, 6))
+print(number_ladder_checker(203, 959, 6))
+print(number_ladder_checker(404, 777, 6))
 
 

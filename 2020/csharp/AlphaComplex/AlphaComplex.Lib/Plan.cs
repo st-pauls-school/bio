aa2c4ca547f155af22bce9472bf99fb0427ec74e
spscah <cah@stpaulsschool.org.uk>
2020-01-21 16:39:17 +0000

    Incomplete 2020 Q2

---
+++ 2020/csharp/AlphaComplex/AlphaComplex.Lib/Plan.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaComplex.Lib
+{
+    public class Plan
+    {
+        readonly IList<Room> _rooms;
+        Room _currentRoom;
+
+        public Plan(string v)
+        {
+            _rooms = new List<Room>();
+            SetupRooms(v);
+            _currentRoom = _rooms[0];
+            
+        }
+
+        public List<string> Connections { get; }
+
+        public string Moves(int v1)
+        {
+            for(int i = 0; i < v1; ++i)
+                _currentRoom = _currentRoom.WhereNext();
+            return _currentRoom.Name;
+        }
+
+        void SetupRooms(string v)
+        {
+            for (int i = 0; i < v.Length; ++i)
+                _rooms.Add(new Room(('A' + i).ToString(), i==0));
+            // todo: build the plan here 
+        }
+    }
+
+    class Room
+    {
+        readonly string _name;
+        IDictionary<Room, bool> _oddExits;
+        bool _oddVisited;
+
+        internal string Name { get { return _name; } }
+
+        internal Room(string name, bool initialOdd = false)
+        {
+            _name = name;
+            _oddVisited = initialOdd;
+            _oddExits = new Dictionary<Room, bool>();
+        }
+
+        internal void AddConnection(Room r)
+        {
+            _oddExits.Add(r, true);
+        }
+
+        internal Room WhereNext()
+        {
+            Room rv = _oddVisited
+                ? _oddExits
+                    .Select(kvp => kvp.Key)
+                    .OrderBy(k => k.Name)
+                    .First()
+                : _oddExits
+                    .Where(kvp => !kvp.Value)
+                    .Select(kvp => kvp.Key)
+                    .OrderBy(k => k.Name)
+                    .First();
+            _oddExits[rv] = !_oddExits[rv];
+            _oddVisited = !_oddVisited;
+            return rv;
+        }
+
+        internal string Connections
+        {
+            get
+            {
+                return string.Join("", _oddExits.Select(kvp => kvp.Key).OrderBy(k => k));
+            }
+        }
+    }
+}
---
+++ 2020/csharp/AlphaComplex/AlphaComplex.Tests/PlanTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using AlphaComplex.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlphaComplex.Tests
+{
+    [TestClass]
+    public class PlanTests
+    {
+        [TestMethod]
+        public void TestGiven()
+        {
+            Plan p = new Plan("A");
+            CollectionAssert.AreEqual(new List<string> { "BC", "A", "A" }, p.Connections);
+            Assert.AreEqual("B", p.Moves(1));
+            Assert.AreEqual("B", p.Moves(2));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme2()
+        {
+            Plan plan = new Plan("C");
+            CollectionAssert.AreEqual(new List<string> { "C", "C", "AB" }, plan.Connections);
+            Assert.AreEqual("B", plan.Moves(4));
+            Assert.AreEqual("B", plan.Moves(8));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme3()
+        {
+            Plan plan = new Plan("BC");
+            CollectionAssert.AreEqual(new List<string> { "B", "AC", "BD", "C" }, plan.Connections);
+            Assert.AreEqual("C", plan.Moves(4));
+            Assert.AreEqual("D", plan.Moves(9));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme4()
+        {
+            Plan plan = new Plan("BBACC");
+            CollectionAssert.AreEqual(new List<string> { "BC", "ADE", "AFG", "B", "B", "C", "C" }, plan.Connections);
+            Assert.AreEqual("F", plan.Moves(10));
+            Assert.AreEqual("G", plan.Moves(1010));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme5()
+        {
+            Plan plan = new Plan("ABCD");
+            CollectionAssert.AreEqual(new List<string> { "BE", "AC", "BD", "CF", "A", "D" }, plan.Connections);
+            Assert.AreEqual("C", plan.Moves(50));
+            Assert.AreEqual("D", plan.Moves(51));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme6()
+        {
+            Plan plan = new Plan("FEDC");
+            CollectionAssert.AreEqual(new List<string> { "F", "E", "DF", "CE", "BD", "AC" }, plan.Connections);
+            Assert.AreEqual("C", plan.Moves(2020));
+            Assert.AreEqual("B", plan.Moves(876543));
+        }
+
+        [TestMethod]
+        public void TestMarkScheme7()
+        {
+            Plan plan = new Plan("AABGB");
+            CollectionAssert.AreEqual(new List<string> { "BCD", "AFG", "A", "A", "G", "B", "BE" }, plan.Connections);
+            Assert.AreEqual("A", plan.Moves(5000));
+            Assert.AreEqual("B", plan.Moves(9999));
+        }
+        [TestMethod]
+        public void TestMarkScheme8()
+        {
+            Plan plan = new Plan("CDCDCD");
+            CollectionAssert.AreEqual(new List<string> { "C", "D", "ADEG", "BCFH", "C", "D", "C", "D" }, plan.Connections);
+            Assert.AreEqual("D", plan.Moves(2020));
+            Assert.AreEqual("H", plan.Moves(876543));
+        }
+        [TestMethod]
+        public void TestMarkScheme9()
+        {
+            Plan plan = new Plan("FFFFFFFF");
+            CollectionAssert.AreEqual(new List<string> { "F", "F", "F", "F", "F", "ABCDEGHIJ", "F", "F", "F", "F" }, plan.Connections);
+            Assert.AreEqual("J", plan.Moves(512));
+            Assert.AreEqual("A", plan.Moves(1022));
+        }
+    }
+}

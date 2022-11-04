34a3f19c1d1e0ec109cb123cca6cf6a86b47443e
spscah <cah@stpaulsschool.org.uk>
2022-11-04 12:13:56 +0000

    2000-q1

---
+++ 2000/q1/BasicsOfIteration/BasicsOfIteration.App/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfIteration.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimesTableF(12, 12);
+            TimesTableW(12, 12);
+
+            //foreach ((int key, int value) p in Enumerate(new List<int> { 1, 2, 3, 4, 5 }))
+            //{
+            //    Console.WriteLine($"{p.key} {p.value}");
+            //}
+            Console.ReadKey();
+        }
+
+        static void TimesTableF(uint n, uint m)
+        {
+            int padA = (int)Math.Ceiling(Math.Log10(m));
+            int padB = (int)Math.Ceiling(Math.Log10(n * m));
+            for (int i = 1; i <= m; ++i)
+            {
+                string s = string.Format("{0,1}", i, -padA);
+                Console.WriteLine($"{s} * {n} = {(i * n)}");
+            }
+        }
+        static void TimesTableW(uint n, uint m)
+        {
+            int padA = (int)Math.Ceiling(Math.Log10(m));
+            int padB = (int)Math.Ceiling(Math.Log10(n * m));
+            int i = 1;
+            do
+            {
+                Console.WriteLine($"{i.ToString().PadLeft(padA)} * {n} = {(i * n).ToString().PadLeft(padB)}");
+            } while (i++ < m);
+        }
+
+        static IEnumerable<(int, T)> Enumerate<T>(List<T> items)
+        {
+            for(int i = 0; i < items.Count; ++i)
+            {
+                yield return (i, items[i]);
+            }
+        }
+
+    }
+}
---
+++ 2000/q1/Passwords.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+Debug.Assert(!Checker("APPLE"));
+Debug.Assert(!Checker("CUCUMBER"));
+Debug.Assert(Checker("ONION"));
+Debug.Assert(Checker("APRICOT"));
+
+
+
+static bool Checker(string s)
+{
+    int sublength = 1;
+    while(sublength <= s.Length/2)
+    {
+        for(int i = 0; i + sublength + sublength < s.Length; ++i)
+        {
+            if (s.Substring(i, sublength) == s.Substring(i + sublength, sublength))
+                return false;
+        }
+        sublength++;
+    }
+    return true;
+
+}
